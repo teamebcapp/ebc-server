3b1897760943ea061d4d8a049e8c85604778e5a2
devwoong <chl_woong159@naver.com>
2019-02-03 23:25:14 +0900

    명함 교환 table추가 쿼리

--- db/table.sql
+++ db/table.sql
@@ -3,6 +3,8 @@ CREATE SCHEMA info;
 CREATE SEQUENCE info.user_user_seq_seq;
 
 CREATE SEQUENCE info.business_card_bc_seq_seq;
+
+CREATE SEQUENCE info.owner_bc_owner_seq_seq;
 /**********************************/
 /* Table Name: info.user */
 /**********************************/
@@ -40,9 +42,27 @@ CREATE TABLE info.business_card(
   CONSTRAINT IDX_business_card_1 UNIQUE (bc_seq)
 );
 
+CREATE TABLE info.owner_bc(
+		owner_seq INT NOT NULL default nextval('info.owner_bc_owner_seq_seq'::regclass),
+		owner_bc_seq INT,
+		owner_user_id VARCHAR(256),
+		name VARCHAR(128),
+		company VARCHAR(128),
+		depart VARCHAR(256),
+		team VARCHAR(128),
+		position VARCHAR(128),
+		duty VARCHAR(128),
+		phone VARCHAR(16),
+		tel VARCHAR(16),
+		fax VARCHAR(32),
+		address VARCHAR(512),
+		email VARCHAR(256),
+  FOREIGN KEY (owner_user_id) REFERENCES info.user (user_id),
+  CONSTRAINT IDX_owner_bc_1 UNIQUE (owner_seq)
+);
 
 CREATE INDEX IDX_user_2 ON info.user (user_id);
 
 CREATE INDEX IDX_business_card_2 ON info.business_card (user_id);
 
-
+CREATE INDEX IDX_owner_bc_2 ON info.owner_bc (owner_user_id);
