7632d62530a36743b627acdd91028927f4de1067
devwoong <chl_woong159@naver.com>
2019-02-07 23:56:44 +0900

    패스워드 변경 컨트롤러 추가

--- user/controller.go
+++ user/controller.go
@@ -89,6 +89,7 @@ func PutUser(w http.ResponseWriter, r *http.Request) {
 	w.Write(result)
 }
 
+// GetUserDuplicate 아이디 중복체크
 func GetUserDuplicate(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	w.Header().Add("Access-Control-Allow-Origin", "*")
@@ -115,4 +116,37 @@ func GetUserDuplicate(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 500)
 	}
 	w.Write(result)
+}
+
+// PutUserPassword 패스워드 변경
+func PutUserPassword(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+
+	var modifyPassword UserPassword
+	err := json.NewDecoder(r.Body).Decode(&modifyPassword)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), 500)
+	}
+	var resultUser User
+	postgres.PostgresConn.Where("user_id = ? AND password = ?", modifyPassword.UserId, modifyPassword.PrevPassword).First(&resultUser)
+
+	var result []byte
+	if resultUser.UserSeq == 0 {
+		log.Println(modifyPassword.UserId, " : 패스워드 변경 실패!!")
+		result, err = utils.ObjectToJsonByte(common.BaseResult{"패스워드 확인", "200", 0})
+	} else {
+		resultUser.Password = modifyPassword.Password
+		postgres.PostgresConn.Save(&resultUser)
+		result, err = utils.ObjectToJsonByte(common.BaseResult{"success", "200", 1})
+		log.Println(resultUser.UserId, " : 패스워드 변경 성공!!")
+	}
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), 500)
+	}
+	w.Write(result)
+
 }
