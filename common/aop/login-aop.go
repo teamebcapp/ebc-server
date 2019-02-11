1ba03633cbcd22c83fff6c95711c399bfad3d1c7
devwoong <chl_woong159@naver.com>
2019-02-11 23:59:40 +0900

    aop추가 현재 사용 안함

---
+++ common/aop/login-aop.go
@@ -0,0 +1,37 @@
+package myAop
+
+import (
+	"fmt"
+
+	"github.com/gogap/aop"
+)
+
+type LoginAop struct {
+}
+
+// use join point to get Args from real method
+func (p *LoginAop) Before(jp aop.JoinPointer) {
+	username := ""
+	jp.Args().MapTo(func(u, p string) {
+		username = u
+	})
+
+	fmt.Printf("Before Login: %s\n", username)
+}
+
+// the args is same as Login
+func (p *LoginAop) After(username, password string) {
+	fmt.Printf("After Login: %s %s\n", username, password)
+}
+
+// use join point to around the real func of login
+func (p *LoginAop) Around(pjp aop.ProceedingJoinPointer) {
+	fmt.Println("@Begin Around")
+
+	ret := pjp.Proceed("fakeName", "fakePassword")
+	ret.MapTo(func(loginResult bool) {
+		fmt.Println("@Proceed Result is", loginResult)
+	})
+
+	fmt.Println("@End Around")
+}
--- github.sh
+++ github.sh
@@ -3,4 +3,3 @@ go get github.com/jinzhu/gorm
 go get github.com/lib/pq
 go get github.com/lib/pq/hstore
 go get github.com/gorilla/schema
-go get github.com/gogap/aop
